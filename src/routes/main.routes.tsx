6cab76b458b66bdd1348f79e7a533f4f7c946f8f
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-08-19 23:59:24 +0800

    chore: add children for the discover

--- src/routes/main.routes.tsx
+++ src/routes/main.routes.tsx
@@ -1,16 +1,32 @@
 import App from "@/App";
 import SigninPage from "@/pages/auth/SigninPage";
 import SignupPage from "@/pages/auth/SignupPage";
+import DiscoverListPage from "@/pages/discover/DiscoverListPage";
+import DiscoverOverviewPage from "@/pages/discover/DiscoverOverviewPage";
 import DiscoverPage from "@/pages/discover/DiscoverPage";
 import HomePage from "@/pages/homepage/HomePage";
+import MangaDetailPage from "@/pages/manga/MangaDetailPage";
 import type { RouteObject } from "react-router-dom";
+import { mangaDetailsLoader } from "./loaders/manga.loaders";
 
 const main_routes: RouteObject = {
   path: "/",
   element: <App />,
   children: [
     { index: true, element: <HomePage /> },
-    { path: "discover", element: <DiscoverPage /> },
+    {
+      path: "discover",
+      element: <DiscoverPage />,
+      children: [
+        { index: true, element: <DiscoverOverviewPage /> },
+        { path: ":type", element: <DiscoverListPage /> },
+        {
+          path: ":type/:id",
+          element: <MangaDetailPage />,
+          loader: mangaDetailsLoader,
+        },
+      ],
+    },
     { path: "signin", element: <SigninPage /> },
     { path: "signup", element: <SignupPage /> },
   ],
